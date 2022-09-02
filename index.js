0e8dcfb2afa707b97742183a2e228eb447b96a35
Andrew Grenon <andrew.grenon@gmail.com>
2022-09-03 11:20:20 +1200

    Commit init

---
+++ index.js
@@ -0,0 +1,28 @@
+let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fight-button")
+let winnerButton = document.getElementById("winner-button")
+let gameBox = document.getElementById("gamebox")
+
+fightButton.addEventListener("click", function() {
+  gameBox.style.animation = "rotate 1s";
+  let indexOne = Math.floor(Math.random() *fighters.length);
+  let indexTwo = Math.floor(Math.random() *fighters.length);
+  stageEl.textContent = `${fighters[indexOne]} V ${fighters[indexTwo]}`
+  let choices = [fighters[indexOne], fighters[indexTwo]]
+
+  winnerButton.addEventListener("click", function() {
+  gameBox.style.animation = "rotate 10s linear";
+  let dice = Math.floor(Math.random() *2);
+   stageEl.textContent = `${choices[dice]} wins!`;
+})
+})
+
+
+
+
+
+
+
+
